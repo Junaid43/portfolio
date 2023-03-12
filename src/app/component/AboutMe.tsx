e809532a7adde7cbac128755d4885e56c6dfc57f
Muhammad Junaid Tariq <muhammad.junaid434@gmail.com>
2023-03-12 12:54:03 +0500

    new changes certification

--- src/app/component/AboutMe.tsx
+++ src/app/component/AboutMe.tsx
@@ -9,7 +9,9 @@ export default function AboutMe({ data }: { data: any }) {
         <h2 className="pb-2 bold font-bold uppercase">{title}</h2>
         <div>
           {body?.map((el: string, i: any) => (
-            <p key={`${id}_${i}`} className="pb-2">{el}</p>
+            <p key={`${id}_${i}`} className="pb-2">
+              {el}
+            </p>
           ))}
         </div>
       </section>
--- src/app/component/Certification.tsx
+++ src/app/component/Certification.tsx
@@ -12,10 +12,7 @@ export default function Certification({ data }: { data: any }) {
         {details.map(
           ({ title, location }: { title: string; location: string }) => (
             <div className="bg-grey px-6 py-6 border-t-[6px] border-blue mb-6">
-              <li
-                key={`${id}_${title}`}
-                className="text-black bold text-xl list-none"
-              >
+              <li key={`${id}`} className="text-black bold text-xl list-none">
                 {title} <span className="text-base">({location})</span>{" "}
               </li>
             </div>
